2bce95b49a9dde048117411ac5e1f3ea81349ed5
Deepak <deepak.v@makoitlab.com>
2023-11-04 21:21:52 +0530

    default exports added

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
 export { default as AuthProvider } from "./AuthProvider";
+export { default as ProfileMenu } from "./ProfileMenu";
